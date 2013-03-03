1b19a8c8953883154540848e9bb895f93cb07d57
nruiz <nruiz@tuenti.com>
2013-03-03 12:27:24 +0100

    Scc, good answers for test cases 2 and 3

--- algorithms1/week4/scc.py
+++ algorithms1/week4/scc.py
@@ -6,33 +6,50 @@
 # if there are 3 SCC with 3 nodes plus no more
 import csv as csv
 
-#depth first search,returns the explored set
-def DFS(Q,node):
-	E = set() ;#explored set
+# depth first search,returns the explored set
+# Q is the graph
+# l leader for cluster,only relevant on step 2
+def DFS(Q,node,calculateTimes,leader=None):
+	global t
 	# The list methods make it very easy to use a list as a stack, 
 	# where the last element added is the first element retrieved LIFO
-	# To add an item to the top of the stack, use append(). To retrieve an item from the top of the stack, use pop() without an explicit index. 
+	# To add an item to the top of the stack, use append(). 
+	# To retrieve an item from the top of the stack, use pop() without an explicit index. 
 	stack = []
 	stack.append(node)
 	while len(stack) >0:
+		unexploredChildren = False;
+		node = stack.pop();
+		if (leader!=None) and (node not in L[leader]):
+			# using a set as we might pass through
+			# here witha node more than once
+			L[leader].add(node);
+		
 		E.add(node)
 		#print "exploring " + str(node);
 		#print Q[node]
 		for v in Q[node]:
 			#print v
 			if not (v in E):
+				#print "adding "+str(v)+" to stack"
+				# append 1 at the time otherwise is not depth first search
+				# we need to calculate finishing times,thus do not remove base
+				# node from stack until all its children have been removed
+				stack.append(node);
 				stack.append(v);#appends to the front
-		node = stack.pop() #pops from the front
+				unexploredChildren = True;
+				break;
 
-	return E
+		if not unexploredChildren and calculateTimes:
+			t = t+1;
+			F[t] = node 
+			#print "finished exploring " +str(node)
+		#print stack
 
+################################ main program ######################
 
-
-	
-
-
-#f = open('testCase1.txt');
-f = open('./scc.txt');
+f = open('testCase3.txt');
+#f = open('./scc.txt');
 reader = csv.reader(f,delimiter=' ', quoting=csv.QUOTE_NONE)
 
 
@@ -57,8 +74,65 @@ for row in reader:
 	GR[head].append(tail);
 
 
-sourceVertex = 1
-E = DFS(G,sourceVertex);
-print " From source vertex " + str(sourceVertex)
-print " We found "+str(len(E))+" nodes";
+
+# remember we start at the bigest vertex
+sourceVertex = max(G.keys());
+
+#finishing times,stored like:  F[time] = node
+F={}
+
+# keeping track of explored
+E = set() 
+
+# global variable for finished times
+t = 0; 
+
+
+# step 1, run DFS in the reverse graph for all nodes
+# calculating finishing times
+for vertex in range(sourceVertex,1,-1):
+	# DFS updates the explored set (global) and finished times (global)
+	if vertex not in E:
+		DFS(GR,vertex,True);
+		print " From source vertex " + str(vertex)
+		print " We found "+str(len(E))+" nodes";
+
+
+
+# step 2, run DFS in decreasing order of finishing times
+# calculating leaders
+
+# reversing F to print so F[node] = time
+#FR = dict (zip(F.values(),F.keys()))
+#print "Times per node:"
+#print FR
+
+# storing cluster leaders and nodes for debugging puposes
+# remove if time takes too much
+L = {} 
+
+maxTime = max(F.keys());
+E = set();
+for t in range(maxTime,1,-1):
+	# explore and choose leaders
+	vertex = F[t]
+	#print "exploring vertex "+str(vertex)
+	if vertex not in E:
+		leader = vertex
+		L[leader] = set()
+		DFS(G,vertex,False,leader)
+		#substitute by length of set
+		L[leader] = len(L[leader])	
+
+#print "Leaders per node"
+#print L
+
+# answer is the size of the 5 largest SCCs
+# post process
+
+size = L.values()
+size.sort(reverse=True);
+print size
+	
+
 
