e28404af41b569aed9d47d1c75b49fe23ee0f7cf
nruiz <nruiz@tuenti.com>
2013-07-30 00:26:40 +0200

    simple example matrix mutiplication,adding comments

--- meteo/mutiply.py
+++ meteo/mutiply.py
@@ -3,10 +3,12 @@ import sys
 
 # size of the matrix
 # execute like 
-# time python mutiply.py 2
+# >> time python mutiply.py 2
+# to mutiply two matrix of 2 by 2
 # with regular python takes 16 secs to mutiply matrix of 300 elements
 # with pypy it takes about 1 sec
 # for 500 elements it takes about 5 sec
+# with pypy and 1 min for regular python
 #
 
 
