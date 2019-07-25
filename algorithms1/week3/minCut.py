556a9217c77994ddb5c71b87cf1bb9e7ac90b740
Nuria Ruiz <nuria@wikimedia.org>
2019-07-24 21:50:27 -0700

    Better variable naming for minimun cut

--- algorithms1/week3/minCut.py
+++ algorithms1/week3/minCut.py
@@ -1,156 +1,117 @@
-#The file contains the adjacency list representation of a simple undirected graph. 
-#There are 200 vertices labeled 1 to 200. The first column in the file represents the vertex label, 
-#and the particular row (other entries except the first column) tells all the vertices 
-#that the vertex is adjacent to. So for example, the $$6^{th}$$ row looks like : "6	155	56	52	120	......". 
-#This just means that the vertex with label 6 is adjacent 
+#!/usr/binlocal/python
+'''
+#The file contains the adjacency list representation of a simple undirected graph.
+#There are 200 vertices labeled 1 to 200. The first column in the file represents the vertex label,
+#and the particular row (other entries except the first column) tells all the vertices
+#that the vertex is adjacent to. So for example, the $$6^{th}$$ row looks like : "6	155	56	52	120	......".
+#This just means that the vertex with label 6 is adjacent
 #to (i.e., shares an edge with) the vertices with labels 155,56,52,120,......,etc
 
 
-#Your task is to code up and run the randomized contraction algorithm for the min cut problem 
-#and use it on the above graph to compute the min cut.  
-#(HINT: Note that you'll have to figure out an implementation of edge contractions.  
-#Initially, you might want to do this naively, creating a new graph from the old every time 
-#there's an edge contraction.  But you should also think about more efficient implementations.)   
-#(WARNING: As per the video lectures, please make sure to run the algorithm many times 
-#with different random seeds, and remember the smallest cut that you ever find.)  
+#Your task is to code up and run the randomized contraction algorithm for the min cut problem
+#and use it on the above graph to compute the min cut.
+#(HINT: Note that you'll have to figure out an implementation of edge contractions.
+#Initially, you might want to do this naively, creating a new graph from the old every time
+#there's an edge contraction.  But you should also think about more efficient implementations.)
+#(WARNING: As per the video lectures, please make sure to run the algorithm many times
+#with different random seeds, and remember the smallest cut that you ever find.)
 #Write your numeric answer in the space provided.  So e.g., if your answer is 5, just type 5 in the space provided
 
-import csv as csv
-import random as random
-import copy as copy
+Sample file:
+    1 2 4
+    2 1 3 4
+    3 2 4
+    4 1 2 3
+
+'''
+import sys
+import random
+import copy
+
+# graph is going to be a hash:
+# in which every entry is a vertex and the value is a list that represent the vertexes is linked to
+# graph[1] = [2,3,4,5]
+
+def buildGraphFromFile(_file):
+    graph = {}
+    f = open(_file)
+    for l in f:
+        items = map(int, l.split())
+        vertex = items[0]
+        edges = items[1:len(items)]
+        graph[vertex] = edges
+    return graph
 
 
+def computeMinimunCut(graph):
+    # if we only have two vertexes we are done
+    if len(graph.keys()) <= 2:
+        return graph
 
-def minCut(G,E):
+    # build list of edges to pick one at random
+    E = []
 
-	Q = copy.deepcopy(G);
-	
-	s = random.sample(E,1);
-	u = s[0][0]
-	v = s[0][1]
-	# rebinding list
-	_E = [(i, j) for i, j in E if i!=u and j!=v]
-	
-	keys = Q.keys();
-	# kind of like union find
-	# list of each vertex and its cluster leader
-	C = {}
+    for k in graph.keys():
+        for e in graph[k]:
+            edge = (k,e)
+            E.append(edge)
 
-	while len(keys) >2:
-		#print "keys"+str(keys)
-		# merge u and v
-		# merge always to u
-		print "merging "+str(u)+" "+str(v)
-		uEdges = Q[u]
-		vEdges = Q[v]
-		edges = vEdges + uEdges
-		del Q[v]
-		#assign leaders
-		C[u] =u
-		C[v] =u
-		# now see if anyone had "v" as a leader, change to u
-		for k in C.keys():
-			if C[k] == v:
-				C[k] = u
-		
-		# go through the edges of u
-		# and find the ones that are on the same cluster 
-		# i.e. u is the leader
-		# and remove them
-		_edges = copy.deepcopy(edges);
-		for e in edges:
-			if C.get(e)!=None and C[e] == u:
-				#print "removing edge"+str(e)
-				_edges.remove(e)
-		edges = _edges
-		Q[u] = edges
-		
-		#print Q	
-		#print "union find"
-		#print C
+    t = random.choice(E)
+    v = t[0]
+    v_to_merge = t[1]
+    edges1 = graph[v]
 
-		#now select new u, v 
-		# if we have more than 2 vertexes in our keys
-		keys = Q.keys()
-		if len(keys) ==2:
-			#we are done
-			#print Q
-			break
-		_vertex = v
-		while _vertex==v:
-			s = random.sample(_E,1);
-			#print "u"+str(u)
-			_u = s[0][0]
-			_v = s[0][1]
-			# when picking cases
-			# never contracted before, if contracted get their leaders from C 
-			print "_u and _v"+str(_u)+" "+str(_v)
-			if C.get(_v)==None:
-				v = _v
-			if C.get(_u)==None:
-				u = _u
-			if C.get(_v) != C.get(_u):
-				if C.get(_u)!=None:
-					u =C[_u]
-				if C.get(_v)!=None:
-					v = C[_v]
+    # "collapse" to one vertex
+    merged_vertex_name = v
+    edges2 = graph[v_to_merge]
 
-			# the case C.get(_v) == C.get(_u) 
-			# is the one in which both vertexes have already been merged
-			# remove them from edge list try again
-			# rebinding list
-			_E = [(i, j) for i, j in E if i!=_u and j!=_v]
+    # calculate union of the two edges lists
+    # we are only contracting one edge at a time
+    # this edge is (v,v_to_merge)
+    final_edges = filter(lambda x: x!= v and x!=v_to_merge, list(edges1 + edges2 ))
 
-	minCutNumberOfVertexes = len(Q[keys[0]]) 
-	print ">>>local min"+str(minCutNumberOfVertexes);
-	return minCutNumberOfVertexes 
+    # delete original vertexes
+    del graph[v]
+    del graph[v_to_merge]
 
-f = open('./kargerMinCut.txt');
-reader = csv.reader(f,delimiter='\t', quoting=csv.QUOTE_NONE)
-#f = open('./testCase3.txt');
-#reader = csv.reader(f,delimiter=' ', quoting=csv.QUOTE_NONE)
+    # traverse graph, change references to v and v_to_merge with references to merged_vertex_name
+    for k in graph.keys():
+        tmp_edges  = []
+        # rebuilding whole edges list
+        for edge in graph[k]:
+            if edge == v or edge == v_to_merge:
+                tmp_edges.append(merged_vertex_name)
+            else:
+                tmp_edges.append(edge)
+        # reset edges on current vertex to new list
+        graph[k] = tmp_edges
 
-G = {}
-V = 0
-E =[]; #tuples of edges to pick an edge at random
-for row in reader:
-	V = V+1
-	v = int(row[0])
-	tails = [] # not set as later this structure will hold repeated edges to one vertex
-	l = len(row)
-	for i in range(1,l):
-		w = row[i]
-		if w.isdigit():
-			w = int(w)
-			tails.append(w)
-			t = (v,w);
-			E.append(t);
-	G[v] = tails
+    # enter new vertex
+    graph[merged_vertex_name] = final_edges
+    return computeMinimunCut(copy.deepcopy(graph))
 
-#print E
 
-# now we need to start contracting graph
-keys = G.keys();
+graph = buildGraphFromFile(sys.argv[1])
+#print graph
 
-#print G
+# start number of crossing edges to 2m, m being number of vertexes
+num_vertexes = len(graph.keys())
+crossing_edges = 2 * num_vertexes
 
-minCutNumberOfVertexes = 1000;
+i  =  20
+minimun_cut = graph
 
-counter = 10;
+while i > 0:
+    _graph = copy.deepcopy(graph)
+    local_minimum = computeMinimunCut(_graph)
+    keys = local_minimum.keys();
+    local_min_crossing_edges = len(local_minimum[keys[0]])
+    # now count crossing edges
+    if crossing_edges > local_min_crossing_edges:
+        crossing_edges = local_min_crossing_edges
+        minimun_cut = local_minimum
+    i = i - 1
 
-print "Minimum found in "+str(counter)+" attempts "
 
-while counter>0: 
-	# pick a random starting vertex
-	seed = random.sample(keys,1);
-	seed = seed[0]
-	#print "seed is "+str(seed);
-	minCutNumber = minCut(G,E)
-	#print "The number of vertexes..."
-	#print minCutNumber
-	minCutNumberOfVertexes = min(minCutNumberOfVertexes,minCutNumber)
-	counter = counter-1
-
-print "Final min:"
-print minCutNumberOfVertexes
-
+print ("crossing edges {0}"). format(crossing_edges)
+print minimun_cut
