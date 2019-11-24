595aeb88e7c0adf3eea96cf9990e97b07814dec8
Nuria Ruiz <nuria@wikimedia.org>
2019-11-23 21:44:54 -0800

    Prettier dijistra

--- algorithms2/week4/dijistra.py
+++ algorithms2/week4/dijistra.py
@@ -1,135 +1,75 @@
+#!/usr/local/bin/python
+"""
+Each row consists of the node tuples that are adjacent
+to that particular vertex along with the length of that edge.
+For example, the 6th row has 6 as the first entry indicating
+that this row corresponds to the vertex labeled 6.
+The next entry of this row "141,8200" indicates that
+there is an edge between vertex 6 and vertex 141
+that has length 8200. The rest of the pairs of this row indicate the other vertices adjacent
+to vertex 6 and the lengths of the corresponding edges.
+17      26,1275 45,5114 142,8016        83,4615
+"""
+import sys
+import heapdict
 
-#The first line indicates the number of vertices and edges, respectively. 
+distance = {}
+# nodes connected to this node  list of tuples ( node, edge_value)
+nodes = {}
+INFINITY =  10000
 
-# Each subsequent line describes an edge (the first two numbers are its tail and head, respectively) 
-# 1 14 6" represent an edge from 1 to 14 with length 6
-# tail:1, head:14 weight:6
-# This program uses the heapdict from python for the dijistra implementation
-import csv as csv;
-import heapdict as hpdict;
-INFINITY = 100000;
+# calculate distance from this node to all the others
+def dijistra(origin):
 
-# toString method is not in heapdict
-def heapToString(h):
-	msg =" heap {"
-	for i in h.keys():
-		msg = msg +" "+str(i)+" :"+str(h[i])
-	return msg+"}"
+    queue = heapdict.heapdict()
 
-# heap with distances from s to the rest of nodes in G
-# graph
-# Using a heap from which we 
-# can access items like a hashmap
-# s = source vertex we have added to X set
-def bootstrapHeap(G,s):
- h = hpdict.heapdict();
- for head in T[s]:
-	cost = G[head][s]	
-	h[head] = cost;
+    for n in nodes.keys():
+        queue[n] = INFINITY
 
- #print heapToString(h)
- return h;
+    queue[origin] = 0
 
+    while len(queue) > 0:
+        (current_node, s)  = queue.popitem()
 
-# returns shortest paths from all vertexes to vertex s
-# G graph G[head][tail]= weight
-# T G[tail] = [] , outgoing edges from a tail
+        next_node = None
 
-def dijistra(G,T,s,v):
-	D = []
-	explored = {} #TODO , can we save this explored set and use distances!=INFINITY
-	#initialize all = INFINITY
-	# note array is off is calculating distances from '0'
-	for i in range(v+1):
-		D.append(INFINITY);
-	D[s] = 0
-	explored[s] = 1
-	# if vertext does not have any outfoing edges 
-	# we can return
-	if T.get(s) == None:
-		return D
-	hp = bootstrapHeap(G,s);
-	while len(hp) > 0:
-		smallest = hp.popitem();
-		w = smallest[0];
-		cost = smallest[1];
-		if explored.get(w) != None:
-			continue
+        # update distance to all nodes reachable
+        # by this one
+        for edge in nodes[current_node]:
+            u = edge[0]
+            weight = edge[1]
+            d = distance[current_node] + weight
+            if distance.get(u) is None or distance.get(u) > d:
+                distance[u] = d
+                if queue.get(u) is not None:
+                    queue[u] = d
 
-		#print "Adding vertex into explored:"+str(w)
-		#print "with distance: "+str(cost)
-		D[w]  = cost;
-		explored[w] = 1;
-		# now update values on heap
-		# get all edges outgoing from 'w'
-		# if vertext at head of edge is not explored
-		#	update cost
-		# if head is explored, remove from heap
-		if T.get(w)==None:
-			#print str(w)+" has no outgoing edges"
-			continue
-		for v in T.get(w):
-			#print "---Checking vertext "+str(v)
-			if explored.get(v)==None:
-				# if the node is not the heap yey
-				newCost = D[w]+G[v][w]
-				if hp.get(v)==None or newCost < hp[v]:
-					#only update cost if it is lower
-					hp[v]= newCost
+        # the next node to explore is the one with minimun distance value
+        # poping from heap on next round will get us that node
 
-				#print "---updating cost for vertex "+str(v)
-				#print "----- to cost"+str(hp[v])
-			elif hp.get(v)!=None: 
-				del hp[v]
-		#print heapToString(hp)
-	return D; 
+if __name__=="__main__":
+    f = open(sys.argv[1])
+
+    for l in f:
+        l = l.strip()
+        # splitting on whitespace
+        items = l.split()
+        # undo  tupls
+        main_node = int(items.pop(0))
+        nodes[main_node] = []
+        for i in items:
+            (node, edge) = i.split(",")
+            nodes[main_node].append((int(node), int(edge)))
 
 
-#----------------------------  main program ---------------------
-# Runing Jhonson's algorirthm to find shortest paths
+##############################
+    origin = 1
+    # just choose one destination
+    distance[origin] = 0
 
-#f = open('./g3.txt','rb');
-f = open('./testCase2.txt','rb');
-reader = csv.reader(f,delimiter=' ',quoting=csv.QUOTE_NONE); 
+    #print nodes
+    print "ORIGIN 1"
+    dijistra(origin)
 
-
-firstRow = reader.next();
-
-v = int(firstRow[0]);#number of vertexes
-e = int(firstRow[1]); #number of edges
-
-G = {}
-T = {}
-for row in reader: 
-	#build adjacency list,keeping pointers like:
-	# G[head][tail] = weight
-	# T[tail] = [] , this T lookup will be useful in dijistra
-	# space optimization , if tail has no outgoing edges is not present on T
-	#tail ---->head
-	tail = int(row[0])
-	head = int(row[1])
-	weight = int(row[2])
-	if G.get(tail)==None:
-		G[tail] = {};
-	if G.get(head)==None:
-		G[head] = {}
-	G[head][tail]= weight;
-
-	if T.get(tail)==None:
-		T[tail] = []
-	T[tail].append(head)
-
-
-
-# Dijistra now. 
-# in order to run dijistra we need to be able to look outgoing vertexes
-# from a given one (in G we are storing incoming vertexes to head like
-# G[head][tail]
-# we need a reverse lookup T[tail][head]
-# we need to run it for all vertexes, trying one for now
-sourceVertex = 1
-B = dijistra(G,T,sourceVertex,v)
-
-print "distances from source vertex: "+ str(sourceVertex)
-for i in range(len(B)):
-	print "From: "+str(i)+" "+str(B[i])
+    for d in sorted(distance.keys()):
+        print "To:{0}: {1}".format(d,distance[d])
