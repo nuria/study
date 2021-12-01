00fbe549c06c9116f1cda94b2662d16fdbdd4874
Nuria <nuria@outschool.com>
2021-11-30 21:22:01 -0800

    starting advent

---
+++ advent/2021/advent1.py
@@ -0,0 +1,26 @@
+#!usr/local/bin/python
+
+import sys
+
+lines = open('./out.txt')
+
+lines = map(lambda x: int(x), lines)
+
+prior = lines[0]
+txt = ''
+for i in range(1, len(lines)):
+    height = lines[i] -prior
+    if height > 0:
+        txt = 'increased'
+    else:
+        txt ='decreased'
+    prior = lines[i]
+    print "{0} {1}".format(lines[i], txt)
+
+
+
+
+
+print "hola"
+
+
