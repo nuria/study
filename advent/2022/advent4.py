203a22f61c4ff3aa3259911694f08fab8c19c511
Nuria <nuria@outschool.com>
2022-12-04 18:43:20 -0800

    Advent 4th day

---
+++ advent/2022/advent4.py
@@ -0,0 +1,37 @@
+#!usr/bin
+
+import sys
+_input = sys.argv[1]
+
+def main():
+    f = open (_input)
+   # In how many assignment pairs does one range fully contain the other?
+   # this seems like sets
+   # if the intersection of the two equals one of them 
+    counter = 0
+    for l in f:
+        l=l.strip()
+        (p1, p2) = l.split(',')
+        #print "{0}, {1}".format(p1,p2)
+        (s1,e1) = p1.split('-')
+        s1 = make_set(int(s1), int(e1))
+        (s2,e2) = p2.split('-')
+        s2 = make_set(int(s2), int(e2))
+        #print s1
+        #print s2
+
+        if (s2.intersection(s1) == s1 or s2.intersection(s1) == s2):
+            counter = counter + 1
+           # print "intersection"
+
+    print "counter"
+    print counter
+
+
+def make_set(s,e):
+    return set([i for i in range(s,e+1)])
+
+
+
+if __name__== "__main__":
+    main()
