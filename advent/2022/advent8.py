4a24b0b781f413f4bb038c1ef0b12e65bb8ab242
Nuria <nuria@outschool.com>
2022-12-08 20:22:38 -0800

    Advent day 8

--- advent/2022/advent8.py
+++ advent/2022/advent8.py
@@ -1,7 +1,7 @@
 #!/usr/bin
 
 import sys
-
+import functools
 
 def main():
     _file = open(sys.argv[1])
@@ -20,59 +20,59 @@ def main():
     outside = 2* x_l +2 *(y_l-2)
     print outside 
 
-    # find islands, where all the way up to the edge a tree is shorter 
-    # per every one position
-    visible_trees = 0
+    visibility = []
+
     for x in range(1, x_l-1):
         for y in range(1, y_l-1):
             t = F[y][x]
             
             print "{0}, ({1},{2})". format(t, x,y)
 
-            # if a tree is visible in one direction, it counts as visisble
-            visible = True 
-            for k in range(0, x):
-                if F[y][k] >= t :
-                    visible= False
+            vis = []
+            
+            current_vis = 0
+            # left
+            for k in range(x-1, 0-1, -1):
+                current_vis = current_vis + 1
+                if F[y][k] >= t:
                     break
-            if visible:
-                visible_trees = visible_trees +1 
-                print "{0} is visible on 0,x".format(t)
-                continue
 
-            visible = True
+            vis.append(current_vis)
+            
+            current_vis = 0
+            # right
             for k in range(x+1, x_l):
-                if F[y][k] >= t :
-                    visible= False
+                current_vis = current_vis + 1
+                if F[y][k] >= t:
                     break
-            if visible:
-                print "{0} is visible on x+1".format(t)
-                visible_trees = visible_trees +1 
-                continue
 
-            visible = True
-            for k in range(0,y):
-                if F[k][x] >= t :
-                    visible= False
+            vis.append(current_vis)
+
+            # up
+            current_vis = 0
+            for k in range(y-1,0-1, -1):
+                current_vis = current_vis + 1
+                if F[k][x] >=t:
                     break
-            if visible:
-                print "{0} is visible on 0,y".format(t)
-                visible_trees = visible_trees +1 
-                continue
 
-            visible = True
+            vis.append(current_vis)
+            
+            # down
+            current_vis = 0
             for k in range(y+1,y_l):
-                if F[k][x] >= t :
-                    visible= False
+                current_vis = current_vis + 1
+                if F[k][x] >= t:
                     break
-            if visible:
-                print "{0} is visible on y+1".format(t)
-                visible_trees = visible_trees +1 
-                continue
-        
-    print "visible trees"
-    print outside + visible_trees
+            
+            vis.append(current_vis)
+            
+            print vis
+            visibility. append(functools.reduce(lambda a,b:a*b,vis))
+    
+        print visibility
 
+    print "max visibility"
+    print max(visibility)
 
 if __name__=="__main__":
     main()
