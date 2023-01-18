b7dab14a637efb1de8649c4fcea222ab02b21da7
Nuria <nuria@outschool.com>
2023-01-18 06:48:15 -0800

    Refreshing 5_2

--- EPI/5_2_increment.py
+++ EPI/5_2_increment.py
@@ -1,39 +1,47 @@
 #!usr/local/bin
 
 import sys
+
 import unittest
-import ast
-# get an array that represents a non negative decimal integer
-# and increment in one so [1,2,9] becomes->[1,3,0]
-
-def add_one(n):
-    # we can transform to int add one, and transform to list again
-    n_int = int("".join(map(str,n)))
-    n_int = n_int +1
-
-    print n_int
-    return map(int, list(str(n_int)))
-
-def add_one_carry(n):
-    # ok now let's add like to teh last number
-    # if last number is '9' we need to carry
-    if n[-1] != 9:
-        n[-1] = n[-1] + 1
-        return n
-    # last number is 9
-    return add_one_carry(n[0:len(n)-1]) + [0]
 
 
-class testing(unittest.TestCase):
+class TestSuite(unittest.TestCase):
 
     def test_happy_case(self):
-        l_plus_one = add_one_carry([1,2,8])
-        self.assertEqual([1,2,9], l_plus_one)
+        result = add([1,2,9])
+        self.assertEqual(result, [1,3,0])
 
-    def test_carry_case(self):
-        l_plus_one = add_one_carry([1,2,9])
-        self.assertEqual([1,3,0], l_plus_one)
 
-if __name__== "__main__":
+def add(_input):
+    print "input"
+    print _input
+    # from [1, 2, 9] increment to [1,3,0]
+    # brute force will be converting to integer and increment and to string again
+    # but there is gotta be a better way
+    
+    # you can increment from the back and "carry over" to the front 
+
+    #iterating the array in reverse
+    carry = True
+    
+    for i in reversed(range(0, len(_input))):
+        if carry:
+            _input[i] = _input[i] + 1 # 9-> 10/ 2->3
+            if _input[i] > 9: # 10 -> 9
+                _input[i] = 0
+                carry = True # true
+            else:
+                carry = False
+
+    return _input
+
+def main():
+    _input = eval(sys.argv[1])
+    print  add(_input)
+
+
+
+if __name__=="__main__":
+    # this interferes with test execution  uncomment to run
+    #main() 
     unittest.main().result
-
