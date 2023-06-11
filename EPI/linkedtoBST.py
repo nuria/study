4fee41c19041fa0e11b4fd888bca0a71c8544953
Nuria <nuria@outschool.com>
2023-06-11 07:46:39 -0700

    Refreshing problem

--- EPI/linkedtoBST.py
+++ EPI/linkedtoBST.py
@@ -9,18 +9,11 @@ class LItem():
         self.left = left
 
     def __repr__(self):
-        return "<- {0}->".format(self.value)
-
-# this defines a node for a bst
-class Node():
-    def __init__(self, value, right=None, left=None):
-        self.value = value
-        self.right = right
-        selft.left = left
-
-    def __repr__(self):
-        "L:{2}<-{0}->R:{1}".format(self.value, self.right, self.left)
-
+        #"{1}<- {0}->{2}".format(self.value, self.left, self.right)
+        
+        #r = "{0}->{1}".format(self.value, self.right)
+        l= "{1}<-{0}".format(self.value, self.left)
+        return l
 
 def _len_list(tail):
     #returns length of a linked list, it needs to traverse list to do so
@@ -34,17 +27,28 @@ def _len_list(tail):
 
 
 def main():
-    head =LItem(2)
-    node3 = LItem(3, head)
-    node5 = LItem(5,node3)
-    node7= LItem(7, node5)
-    node11 =LItem(11,node7)
-
-    head.right = node3
+    node2  = LItem(2)
+    node3  = LItem(3)
+    node5  = LItem(5)
+    node7  = LItem(7)
+    node11 = LItem(11)
+    
+    node2.right = node3
     node3.right = node5
     node5.right = node7
-
+    node7.right = node11
     tail = node11
+    head = node2
+    
+
+    node11.left = node7
+    node7.left = node5
+    node5.left = node3
+    node3.left = head
+   
+    tail.right = None
+    head.left = None
+
 
     def build_tree(tail):
         # L: linked list 
@@ -52,41 +56,43 @@ def main():
         # TODO: we are calculating this over and over, optimize 
         l = _len_list(tail)
 
-        print "len of list :{0}".format(l)
-
         if l == 1:
             return l
 
         if l == 2:
-            left = tail.left
-            left.left = None
-            left.right =None
+            head = tail.left
+            head.left = None
+            head.right =None
+            print(tail)
             return tail
         
         if l == 3:
-            right = tail
             root = tail.left
-            left =  root.left
-            root.right = right
-            root.left = left
-            
-            right.right = None
-            right.left = None
-            left.right= None
-            left.left = None
-            
+            head = root.left
+            root.right = tail
+            root.left = head
+           
+            head.left = None
+            head.right = None
+            tail.right = None
+            tail.left = None
+            print(root)            
             return root
 
         # l 4 or more:
         # divide problem
         m = l/2
-        i = 1
+        i = 0
 
+        # find root
+        # we really could find this when calculating list length
         root = tail
         while(i <=m):
             root = tail.left
             i += 1
-        
+        print "root node: {0}".format(root)
+
+
         right_node = root.right
         # cut right side of tree off
         if right_node is not None:
@@ -106,14 +112,25 @@ def main():
         return root
 
         
-        
+    def print_tree(node):
+        txt = ''
+        print(node)
+        if node.left is not None:
+            txt += print_tree(node.left)
 
+        txt += ' ' + str(node.value)
+       
 
-    print str(head)
+        if node.right is not None:
+            txt += print_tree(node.right)
+
+        return "[{0}]".format( txt)
+
 
     tree = build_tree(tail)
+    print(tree)
 
-    print str(tree)
+    print print_tree(tree)
 
 if __name__=="__main__":
     main()
