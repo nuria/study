18a08dfa0ebd805b82b692bb34b5c36bc39100ab
Nuria <nuria@outschool.com>
2023-06-21 06:17:35 -0700

    Rfreshing graph problem

--- EPI/18_transform.py
+++ EPI/18_transform.py
@@ -1,34 +1,67 @@
-#!usr/local/bin
+#/usr/local/bin
+
+def main():
+    D =['bad','cot','dog', 'dag','dot','cat']
+    s = 'cat'
+    t = 'dog'
+
+    # treat strings as nodes on an undirected graph with an edge between them
+    # if they differ in one char
+
+    # graph
+    # each entry is an array of edges
+    # with the right representation this is a BFS
+    G = {}
+    v = {}
+    print (D)
+
+    for n in D:
+        G[n] =[]
+        v[n] = 0
+
+    # building the grap is o(n2) if we do not do something smarter
+    for w in D:
+        # compare with the rest of the words
+        # set intersection
+        for w2 in D:
+            if w ==w2:
+                continue
+            difference = 0
+            # same functionality using set intersection
+            #if len(set(w2).intersection(w)) >= 2:
+            #     G[w].append(w2)
+            for c in w2:
+                if c not in set(w):
+                    difference+=1
+            if difference < 2:
+                G[w].append(w2)
 
 
-import collections
 
-def transform(D, s, t):
-    # D is a dictionary of words
+    print (G)
+    
+    import collections as c 
+    q = c.deque([])
+    q.append(s)
 
-    d = collections.deque()
+    path = 0
+    # BFS
+    while len(q) > 0:
+        node = q.popleft()
+        v[node] = 1
+        path +=1
+        if node == t:
+            # this would be the shortest sequence
+            print(path)
+            return
+        else:
+            for g in G[node]:
+                if v[g] < 1:
+                    q.append(g)
 
-    d.append(s)
+        print(q)
+    # no path
+    print("-1")
 
-    sequence  = []
-    sequence.append(s)
-
-    while len(q) >0:
-        word = q.popleft()
-        sequence.append(word)
-        if word == t:
-            return sequence
-
-        for i in range(0, len(word)):
-            for l in string.ascii_lowercase:
-                new_word = word[:i] + c word[i+1:c]
-                if new_word in D:
-                    q.append(new_word)
-
-        return  -1
-
-
-
-
-if __name__== "__main__":
-
+if __name__=="__main__":
+    main()
