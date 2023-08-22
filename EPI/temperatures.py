3c09bf89a4bc35e4f7839d41fe9a95a73256c5bc
Nuria <nuria@outschool.com>
2023-08-22 07:18:46 -0700

    Adding problem

--- EPI/temperatures.py
+++ EPI/temperatures.py
@@ -6,6 +6,7 @@ of days you have to wait after the ith day to get a warmer temperature. If there
 which this is possible, keep answer[i] == 0 instead.
 """
 import sys
+import unittest 
 
 def main():
     # [73,74,75,71,69,72,76,73]
@@ -26,30 +27,24 @@ def main():
     print (answer)
 
     # let's use a queue 
-    # we can reverse an use a normal list
-    data = reversed(data)
+    # but rather than keeping values let's use the ideas of the monotonic stack
+    # and keep indexes so we can "backtrack" and fill in pass values we have already pass
 
     answer2 = [0] * len(data)
-    # queue where we temporaryly store looked up temps
-    tmp = []
-    
-    i = 0
-    while len(l) > 0:
-        # get 1st element 
-        t = data.pop()
-           
-        while (data[-1] < t):
-            tmp.append(data.pop())
 
-        data[i] = len(tmp)
-        # now restore elements
-        while(len(tmp) > 0):
-            data.append(tmp.pop())
+    stack = []
+    stack.append(0)
+
+    for i  in range(1, len(data)):
+        value = data[stack[-1]]
+        while data[i] > value and len(stack) >0:
+            answer2[stack.pop()] = i
+
+        # current value is smaller than the ones we are keeping track in the stack
+        stack.append(i)
 
 
-        i = i + 1 
-
-
+    print (answer2)
 
 if __name__=="__main__":
 
