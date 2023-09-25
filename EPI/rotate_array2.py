7617ee52a0309ed2ea20ac64694b5cc4e19401fc
Nuria <nuria@outschool.com>
2023-09-25 07:04:34 -0700

    Adding problem

---
+++ EPI/rotate_array2.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin
+"""
+nums = [1,2,3,4,5,6,7], k = 3
+rotate nums array k steps
+output: [5,6,7,1,2,3,4]
+"""
+import sys 
+
+def main():
+    
+
+    A = eval(sys.argv[1])
+    k = int(sys.argv[2])
+
+    l = len(A) 
+   
+    buffer = A[l-k:]
+
+    # we have a buffer of size k where the last k elements will go
+    # we move other elements forward k steps
+    # this is o(n) on time by o(k) on space
+    
+    for i in range(0, l-k):
+        A[i+k] = A[i]
+
+
+    for i in range(0, k):
+        A[i] = buffer[i]
+
+    print(A)
+
+
+   
+
+if __name__=="__main__":
+    main()
