0a5d0081008f750e969f64c3fda735ac9055ddb3
Nuria <nuria@outschool.com>
2023-11-04 17:07:11 -0700

    Adding problem

--- misc/fake_streaming.py
+++ misc/fake_streaming.py
@@ -1,106 +1,110 @@
-#!/usr/local/bin
+#!/usr/local/lib
 
-import collections 
+import collections
+
+
+def compute(w):
+    # machine, process, state, time
+    # we have to group data per process 
+    # as it arrives 
+    cache = {}
+    result = {}
+
+    # cache['p1'] = {start:[], end:[]}
+    # this is a possible structure 
+    # does not use data per machine
+    
+    # at the end of processing window events we will have a start-end times per process
+
+    for e in w:
+        (machine, p, status, ts) = e
+        if cache.get(p) is None:
+            cache[p] = {}
+            cache[p]['start']= []
+            cache[p]['end'] =[]
+        cache[p][status].append(ts)
+
+    print (">>>>> window  data ")
+
+    print (cache)
+
+
+    for p in cache.keys():
+        tally = 0
+        # now process data per process
+        start_ts = cache[p]['start']
+        end_ts = cache[p]['end']
+
+        j = 0
+        i = 0
+        n = 0
+        while i < min(len(start_ts), len(end_ts) ) and j < min(len(start_ts), len(end_ts)) :
+            # exit if we run out of data to process
+
+            # possible approaches
+            # this will depend on data shape
+            # is the 1st start before first end? if not 
+            # we need to move with different pointers in each list
+            # do we order the timeseries? 
+            # we decide not to order
+            if start_ts[j] < end_ts[i]:
+                tally = tally + end_ts[i] - start_ts[j]
+                j = j + 1
+                n =+ 1
+            else:
+                while(star_ts[j] > end_ts[i] and j < len(start_ts)):
+                    j = j +  1
+                if j < len(start_ts):
+                    tally = tally + end_ts[i]- start_ts[j]
+                    n+=1 
+
+            i = i +1
+
+            result[p] = tally/n
+
+    return result
+
+
 
 def main():
-
-    events = []
     f = open('./data_streaming.txt')
+    # calculate avg time per process
+
+    stream = []
+
     for l in f:
+        # machine, process, status, time
         l = l.strip()
-        (machine, process, state, ts) = l.split()
-        events.append((machine, process, state, float(ts)))
-
-    START = "start"
-    END = "end"
-
-    def window_duration(events):
-        
-        # stores processes and durations
-        d = {}
-        h = {}
-        for e in events:
-           
-            print (h)
-            
-            (machine, process, state, ts) = e
-            key = machine + process
-            
-            if d.get(process) is None:
-                d[process] = []
-
-            if state == START:
-                # append to queue , FIFO
-                if h.get(key) is None:
-                    h[key] = collections.deque()
-                (h[key]).append(ts)
-            else:
-                # compute diference, pop from queue 
-                # does not account for late arriving data 
-                
-                if h.get(key) is None:
-                    continue
-                else:
-                    q = h[key]
-                    start = q.popleft() 
-                    if ts > start:
-                        d[process].append(ts-start)
-        return d
+        (machine, process, status, time) = l.split()
+        stream.append([machine, process, status, float(time)])
 
 
 
-    # first sort by time
-    #(machine, process, state, ts)
-
-    events.sort(key=lambda x: x[3])
-    #print(events)
+    # important is to process just s a window of events
+    # we would do this by time , say 5 minutes
+    # faking here with number of events
 
     WINDOW_SIZE = 10
+
+    # we assume windows overlap so we have windows of 10 values 
+    # that are set every 5 values
     
+    result = []
 
-    averages = {}
-
-    event_window = collections.deque()
-
-    # we are going to compute an average 
-    
-    i = 0
-    while i < len(events):
-        # put data into window
-        while len(event_window) <= WINDOW_SIZE:
-            e = events[i]
-            event_window.append(e)
+    w = collections.deque()
+    i = 0 
+    while i < len(stream):
+        while(len(w)< WINDOW_SIZE and i <len(stream)):
+            w.append(stream[i])
             i = i + 1
-            # now compute with events we have
-        # recomputes everytime
-        d = window_duration(event_window)
-        
-        max_avg_duration = 0
-        max_process = None
-
-        for k in d.keys():
-            diff = d[k] 
-            if len(diff) <1:
-                continue
-            avg = sum(diff)/len(diff)
-            if avg > max_avg_duration:
-                max_avg_duration= avg
-                max_process = k
-
-        print (" {0} {1}".format(max_process, max_avg_duration))
-        
-        # we can move window forward by however many 
-        # this moves it by 10
-
-        if len(event_window) > 0:
-            event_window = []
-        i = i + 1
-
-    
-
-        
-
-
+        # process events
+        print(w)
+        result = compute(w)   
+        print(">>results")
+        print(result)
+        # once process 
+        while (len(w) > 5):
+            w.popleft()
 
 
 
